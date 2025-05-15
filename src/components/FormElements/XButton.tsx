38493566634c4397d32386a9ba183da63744d119
Fırat Yıldız <firatyildiznet@gmail.com>
2025-05-16 00:13:09 +0300

    Added custom form elements

---
+++ src/components/FormElements/XButton.tsx
@@ -0,0 +1,64 @@
+import { ReactElement } from 'react'
+import Link from 'next/link';
+
+interface FireButtonProps {
+    href?: string;
+    label?: ReactElement | string;
+    disabled?: boolean;
+    backgroundColor?:string;
+    textStyle?:string;
+    padding?:string;
+    margin?:string;
+    radius?:string;
+    addStyle?:string;
+    onClick?: () => void;
+    [key: string]: any;
+}
+
+export default function XButton({
+    href,
+    icon: Icon,
+    label,
+    disabled,
+    backgroundColor = '',
+    textStyle = '',
+    padding = '',
+    margin = '',
+    radius = '',
+    addStyle = '',
+    onClick,
+    ...rest
+}: FireButtonProps) {
+    const hoverStyle = backgroundColor == 'white' ? 'opacity-80' : 'bg-gray-800'
+
+    return (
+        <>
+            {href ? (
+                <Link 
+                    href={href}
+                    onClick={onClick}
+                    className={`
+                        w-full ${margin} ${padding} ${backgroundColor} ${textStyle}
+                        hover:${hoverStyle} ${radius}
+                        ${disabled && 'opacity-50 cursor-not-allowed'}
+                        ${addStyle}
+                    `}
+                    {...rest}
+                >{label}</Link>
+            ): (
+                <button
+                    disabled={disabled}
+                    onClick={onClick}
+                    className={`
+                        w-full ${margin} ${padding} ${backgroundColor} ${textStyle}
+                        hover:bg-gray-600 ${radius}
+                        ${disabled && 'opacity-50 cursor-not-allowed'}
+                        ${addStyle}
+                    `}
+                    {...rest}
+                >{label}</button>
+            )}
+        </>
+       
+    )
+}
