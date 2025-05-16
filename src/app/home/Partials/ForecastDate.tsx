c3071ef61326c5f1bd8f43c048d04a86a120db41
Fırat Yıldız <firatyildiznet@gmail.com>
2025-05-16 19:32:21 +0300

    Updated home page for table chart

--- src/app/home/Partials/ForecastDate.tsx
+++ src/app/home/Partials/ForecastDate.tsx
@@ -1,86 +1,18 @@
 'use client'
-import { useMemo, useEffect } from 'react'
+import { useMemo } from 'react'
 import { ColumnDef } from "@tanstack/react-table";
 
-// Helpers
-import { Request } from '@/helpers/Request';
-
 // Components
-import Table from '@/components/DataTable/Table';
-import TableLazy from '@/components/TableLazy';
+import Table from '@/components/Table';
 
 // Other npm packages
-import {useQuery} from '@tanstack/react-query'
 import { HotelStatsTypes } from '@/app/types/hotel-stats';
-import { useSearchParams } from "next/navigation";
 
-interface GetOtelStatsProps {
-    startDate?: string;
-    endDate?: string;
+interface ForecastDateProps {
+    data: HotelStatsTypes[]
 }
 
-function TarihTablo() {
-    
-    const searchParams = useSearchParams();
-
-    const startDate = searchParams.get('start_date') ?? '2024-06-01';
-    const endDate = searchParams.get('end_date') ?? "2024-06-20";
-
-    const isValidDates = useMemo(() => startDate !== '' && endDate !== '', [startDate, endDate]);
-
-    const  getOtelStats = async({
-        startDate = '2024-06-01', 
-        endDate = "2024-06-20"
-    }: GetOtelStatsProps) => {
-        
-        const payload = {
-            db_Id: 9,
-            xRez_Sirket: 9,
-            xBas_Tar: startDate,
-            xBit_Tar: endDate,
-            xtip: 1,
-            kon1: "ALL",
-            kon2: "BB",
-            xchkFis_Fazla_otel_10: 0,
-            bas_Yil: 2022,
-            bit_Yil: 2022,
-            fisrci_Kapalioda_10: 0,
-            xRez_C_W: "C",
-            xSistem_Tarihi: "2024-01-01",
-            xAlis_Tarihi: "2024-01-01",
-            sistem_Bas1: "2020-01-01",
-            sistem_Bit1: "2029-01-01",
-            pmdahil_10: 0,
-            tip_1: "001",
-            xFis_Bela_tutar_10: 0,
-            trace_Dus_10: 0,
-            cev_01: null,
-        };
-
-        const otelStats = await Request({
-          method: 'POST',
-          url: '/Procedure/StpRmforKlasik_2',
-          data: payload
-        });
-
-        return otelStats
-    } 
-
-    const {
-        data: otelStats,
-        isLoading,
-        isError,
-        error,
-    } = useQuery({
-        queryKey: ['otelStats', startDate, endDate],
-        queryFn: () => getOtelStats({ startDate, endDate }),
-        enabled: isValidDates, // sadece iki tarih de varsa çalışsın
-        staleTime: 1000 * 60 * 5,
-        refetchOnWindowFocus: false,
-        refetchOnMount: false,
-        refetchOnReconnect: true,
-        refetchInterval: false,
-    });
+function ForecastDate({ data }: ForecastDateProps) {
 
     const columns: ColumnDef<HotelStatsTypes>[] = useMemo(
         () => [
@@ -330,17 +262,12 @@ function TarihTablo() {
 
     return (
          <div  className="w-full px-4 bg-white rounded-sm ">
-            {!isLoading && otelStats?.value ? (
-                <Table
-                    data={otelStats.value!}
-                    columns={columns}
-                    searchFilter={true}
-                />
-            ): (
-                <TableLazy />
-            )}
+            <Table
+                data={data!}
+                columns={columns}
+            />
         </div>
     )
 }
 
-export default TarihTablo
+export default ForecastDate
